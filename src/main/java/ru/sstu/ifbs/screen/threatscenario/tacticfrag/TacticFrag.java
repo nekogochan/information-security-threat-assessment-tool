2f4f0ce14c0e9663ebd6eef96859e1bc6be74122
GeorgiyO <hiddenpoolgogen@gmail.com>
2022-05-08 21:52:43 +0400

    Добавил фрагменты для сценаррия угроз

---
+++ src/main/java/ru/sstu/ifbs/screen/threatscenario/tacticfrag/TacticFrag.java
@@ -0,0 +1,10 @@
+package ru.sstu.ifbs.screen.threatscenario.tacticfrag;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("gwf_TacticFrag")
+@UiDescriptor("tactic-frag.xml")
+public class TacticFrag extends ScreenFragment {
+}
---
+++ src/main/java/ru/sstu/ifbs/screen/threatscenario/tacticfrag/techniquefrag/TechniqueFrag.java
@@ -0,0 +1,10 @@
+package ru.sstu.ifbs.screen.threatscenario.tacticfrag.techniquefrag;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("gwf_TechniqueFrag")
+@UiDescriptor("technique-frag.xml")
+public class TechniqueFrag extends ScreenFragment {
+}
